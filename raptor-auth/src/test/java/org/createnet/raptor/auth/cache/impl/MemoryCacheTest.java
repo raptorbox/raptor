b859e3f63ab2fbeec0c6b3f054e5d03b59fcac5c
Luca Capra <luca.capra@gmail.com>
2016-05-20 15:42:05 +0200

    add service

---
+++ raptor-auth/src/test/java/org/createnet/raptor/auth/cache/impl/MemoryCacheTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2016 Luca Capra <lcapra@create-net.org>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.createnet.raptor.auth.cache.impl;
+
+import java.util.Map;
+import org.createnet.raptor.auth.AuthConfiguration;
+import org.createnet.raptor.auth.authentication.Authentication;
+import org.createnet.raptor.auth.authorization.Authorization;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Luca Capra <lcapra@create-net.org>
+ */
+public class MemoryCacheTest {
+  
+  MemoryCache instance;
+  
+  public MemoryCacheTest() {
+  }
+  
+  @BeforeClass
+  public static void setUpClass() {
+  }
+  
+  @AfterClass
+  public static void tearDownClass() {
+  }
+  
+  @Before
+  public void setUp() {
+    
+    AuthConfiguration config = new AuthConfiguration();
+    
+    config.cache = AuthConfiguration.Cache.memory.name();
+    config.type = AuthConfiguration.Type.allow_all.name();
+    
+    instance =  new MemoryCache();
+    instance.initialize(config);
+  }
+  
+  @After
+  public void tearDown() {
+    instance.clear();
+    instance = null;
+  }
+
+  @Test
+  public void testScheduledRemoval() throws Exception {
+  
+    String userId = "user";
+    String objectId = "obj";
+    Authorization.Permission perm = Authorization.Permission.Create;
+    
+    boolean value = true;
+    
+    instance.set(userId, objectId, perm, value);
+    
+    assertEquals(value, instance.get(userId, objectId, perm));
+    
+    Thread.sleep(MemoryCache.CachedItem.defaultTTL + 1000);
+    
+    
+    assertNotEquals(value, instance.get(userId, objectId, perm));
+    
+  }
+
+}
