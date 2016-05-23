0524395501c3b2f98d0351ad0c0494a0dab97848
Luca Capra <luca.capra@gmail.com>
2016-05-23 15:04:27 +0200

    fix cors headers version

--- raptor-http-api/src/main/java/org/createnet/raptor/http/filter/CORSResponseFilter.java
+++ raptor-http-api/src/main/java/org/createnet/raptor/http/filter/CORSResponseFilter.java
@@ -35,7 +35,7 @@ public class CORSResponseFilter implements ContainerResponseFilter {
     if (request.getMethod().toUpperCase().equals("OPTIONS")) {
       
       response.getHeaders().add("Access-Control-Allow-Origin", "*");
-      response.getHeaders().add("Access-Control-Allow-Headers", "*");
+      response.getHeaders().add("Access-Control-Allow-Headers", "Authorization,Content-Type,X-Requested-With");
       response.getHeaders().add("Access-Control-Allow-Credentials", "true");
       response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
 
