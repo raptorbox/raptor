ed02cd257eae01cfa6d5eb41d7a708ab37cc7942
Luca Capra <luca.capra@gmail.com>
2016-10-31 15:12:57 +0100

    removed couchbase specific tests

--- raptor-db/src/test/java/org/createnet/raptor/db/couchbase/CouchbaseConnectionTest.java
+++ raptor-db/src/test/java/org/createnet/raptor/db/couchbase/CouchbaseConnectionTest.java
@@ -25,8 +25,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.createnet.raptor.db.Storage;
 import org.createnet.raptor.db.StorageProvider;
 import org.createnet.raptor.db.config.StorageConfiguration;
@@ -44,246 +42,247 @@ import static org.junit.Assert.*;
  * @author Luca Capra <lcapra@create-net.org>
  */
 public class CouchbaseConnectionTest {
-
-    static String bucketKey = "stream";
-    String userId = "dude";
-    String streamId = "sensorstuff";
-
-    static final protected ObjectMapper mapper = Storage.mapper;
-    static final StorageProvider storage = new StorageProvider();
-
-    static Storage.Connection instance = null;
-
-    protected ObjectNode loadData(String filename) {
-
-        try {
-            String filepath = filename + ".json";
-            URL res = getClass().getClassLoader().getResource(filepath);
-
-            if (res == null) {
-                throw new IOException("Cannot load " + filepath);
-            }
-
-            String strpath = res.getPath();
-
-            Path path = Paths.get(strpath);
-            byte[] content = Files.readAllBytes(path);
-
-            return (ObjectNode) mapper.readTree(content);
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
-
-    ;
-  
-  public CouchbaseConnectionTest() {
-    }
-
-    public static Storage.Connection getConnection() {
-
-        if (instance == null) {
-
-            StorageConfiguration config;
-            config = new StorageConfiguration();
-
-            config.type = "couchbase";
-
-            config.couchbase.buckets.put(bucketKey, "test_stream");
-
-            List<List<String>> indexList = new ArrayList();
-
-            List<String> indexFields = new ArrayList();
-            // userId
-            indexFields.add("userId");
-            indexList.add(indexFields);
-
-            // userId + streamId
-            indexFields.clear();
-            indexFields.add("userId");
-            indexFields.add("streamId");
-            indexList.add(indexFields);
-
-            // userId + objectId
-            indexFields.clear();
-            indexFields.add("userId");
-            indexFields.add("objectId");
-            indexList.add(indexFields);
-
-            config.couchbase.bucketsIndex.put(bucketKey, indexList);
-
-            config.couchbase.nodes.add("raptor.local");
-
-            config.couchbase.username = "admin";
-            config.couchbase.password = "password";
-
-            // bootstrap
-            storage.initialize(config);
-            storage.setup(true);
-            storage.connect();
-
-            instance = storage.getConnection(bucketKey);
-
-            assertNotNull(instance);
-        }
-
-        return instance;
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-        // setup & connect
-        getConnection();
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-        // drop test buckets
-        storage.destroy();
-        instance = null;
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
-    protected String getStreamId(ObjectNode record, int time) {
-        String id = record.get("objectId") + "-" + record.get("streamId") + "-" + time;
-        return id;
-    }
-
-    protected int getTime() {
-        return (int) (System.currentTimeMillis() / 1000);
-    }
-
-    protected int getTTL(int time) {
-        return time + (1000 * 60 * 60 * 24 * 90); // +3 months;
-    }
+//
+//    static String bucketKey = "stream";
+//    String userId = "dude";
+//    String streamId = "sensorstuff";
+//
+//    static final protected ObjectMapper mapper = Storage.mapper;
+//    static final StorageProvider storage = new StorageProvider();
+//
+//    static Storage.Connection instance = null;
+//
+//    protected ObjectNode loadData(String filename) {
+//
+//        try {
+//            String filepath = filename + ".json";
+//            URL res = getClass().getClassLoader().getResource(filepath);
+//
+//            if (res == null) {
+//                throw new IOException("Cannot load " + filepath);
+//            }
+//
+//            String strpath = res.getPath();
+//
+//            Path path = Paths.get(strpath);
+//            byte[] content = Files.readAllBytes(path);
+//
+//            return (ObjectNode) mapper.readTree(content);
+//        } catch (IOException ex) {
+//            throw new RuntimeException(ex);
+//        }
+//    }
+//
+//    public CouchbaseConnectionTest() {
+//    }
+//
+//    public static Storage.Connection getConnection() {
+//
+//        if (instance == null) {
+//
+//            StorageConfiguration config;
+//            config = new StorageConfiguration();
+//
+//            config.type = "couchbase";
+//
+//            config.couchbase.buckets.put(bucketKey, "test_stream");
+//
+//            List<List<String>> indexList = new ArrayList();
+//
+//            List<String> indexFields = new ArrayList();
+//            // userId
+//            indexFields.add("userId");
+//            indexList.add(indexFields);
+//
+//            // userId + streamId
+//            indexFields.clear();
+//            indexFields.add("userId");
+//            indexFields.add("streamId");
+//            indexList.add(indexFields);
+//
+//            // userId + objectId
+//            indexFields.clear();
+//            indexFields.add("userId");
+//            indexFields.add("objectId");
+//            indexList.add(indexFields);
+//
+//            config.couchbase.bucketsIndex.put(bucketKey, indexList);
+//
+//            config.couchbase.nodes.add("localhost");
+//
+//            config.couchbase.username = "admin";
+//            config.couchbase.password = "password";
+//
+//            // bootstrap
+//            storage.initialize(config);
+//            storage.setup(true);
+//            storage.connect();
+//
+//            instance = storage.getConnection(bucketKey);
+//
+//            assertNotNull(instance);
+//        }
+//
+//        return instance;
+//    }
+//
+//    @BeforeClass
+//    public static void setUpClass() {
+//        // setup & connect
+//        getConnection();
+//    }
+//
+//    @AfterClass
+//    public static void tearDownClass() {
+//        // drop test buckets
+//        storage.destroy();
+//        instance = null;
+//    }
+//
+//    @Before
+//    public void setUp() {
+//    }
+//
+//    @After
+//    public void tearDown() {
+//    }
+//
+//    protected String getStreamId(ObjectNode record, int time) {
+//        String id = record.get("objectId") + "-" + record.get("streamId") + "-" + time;
+//        return id;
+//    }
+//
+//    protected int getTime() {
+//        return (int) (System.currentTimeMillis() / 1000);
+//    }
+//
+//    protected int getTTL(int time) {
+//        return time + (1000 * 60 * 60 * 24 * 90); // +3 months;
+//    }
+//
 
     @Test
-    public void testSetGet() {
-
-        int time = getTime();
-
-        ObjectNode record = record = loadData("record");
-        record.put("timestamp", time);
-
-        String id = getStreamId(record, time);
-
-        instance.set(id, (JsonNode) record, 0);
-
-        JsonNode json = instance.get(id);
-
-        assertNotNull(json);
-
-        assertTrue(json.has("channels"));
-        assertEquals(json.get("timestamp").asInt(), time);
-
-        instance.delete(id);
-
-    }
-
-    /**
-     * Test of delete method, of class CouchbaseConnection.
-     */
-    @Test
-    public void testDelete() {
-
-        int time = getTime();
-
-        ObjectNode record = record = loadData("record");
-        record.put("timestamp", time);
-
-        String id = getStreamId(record, time);
-
-        instance.set(id, record, 0);
-
-        // load data, ensure is set
-        JsonNode result = instance.get(id);
-        assertNotNull(result);
-
-        instance.delete(id);
-
-        // load data, ensure is gone
-        result = instance.get(id);
-        assertNull(result);
-
-    }
-
-    /**
-     * Test of list method, of class CouchbaseConnection.
-     */
-    @Test
-    public void testList() {
-
-        int count = 5;
-        int timestamp = createRecords(5);
-
-        // list records by userId + streamId
-        BaseQuery query = BaseQuery.queryBy(
-                new BaseQuery.QueryParam("userId", userId),
-                new BaseQuery.QueryParam("streamId", streamId)
-        );
-
-        List<JsonNode> result = instance.list(query);
-
-        assertEquals(result.size(), count);
-
-    }
-
-    /**
-     * Test of list method, of class CouchbaseConnection.
-     */
-    @Test
-    public void testListLimit() throws Exception {
-
-        int count = 5;
-        int timestamp = createRecords(5);
-
-        // list records by userId + streamId
-        BaseQuery query = BaseQuery.queryBy(
-                new BaseQuery.QueryParam("userId", userId),
-                new BaseQuery.QueryParam("streamId", streamId)
-        );
-
-        query.limit = 1;
-        query.setSort("timestamp", ListQuery.Sort.DESC);
-
-        List<JsonNode> result = instance.list(query);
-
-        assertEquals(result.get(0).get("timestamp").asInt(), timestamp);
-
-    }
-
-    private int createRecords(int count) {
-
-        int timestamp = 0;
-
-        for (int i = 0; i < count; i++) {
-
-            int time = getTime();
-
-            ObjectNode record = record = loadData("record");
-
-            timestamp = time + (i * 100);
-
-            record.put("timestamp", timestamp);
-            record.put("userId", userId);
-            record.put("streamId", streamId);
-
-            String id = getStreamId(record, timestamp);
-            instance.set(id, record, 0);
-
-            JsonNode res = instance.get(id);
-            assertNotNull(res);
-
-        }
-
-        return timestamp;
-    }
+    public void testfoo() {}
+//    @Test
+//    public void testSetGet() {
+//
+//        int time = getTime();
+//
+//        ObjectNode record = record = loadData("record");
+//        record.put("timestamp", time);
+//
+//        String id = getStreamId(record, time);
+//
+//        instance.set(id, (JsonNode) record, 0);
+//
+//        JsonNode json = instance.get(id);
+//
+//        assertNotNull(json);
+//
+//        assertTrue(json.has("channels"));
+//        assertEquals(json.get("timestamp").asInt(), time);
+//
+//        instance.delete(id);
+//
+//    }
+//
+//    /**
+//     * Test of delete method, of class CouchbaseConnection.
+//     */
+//    @Test
+//    public void testDelete() {
+//
+//        int time = getTime();
+//
+//        ObjectNode record = record = loadData("record");
+//        record.put("timestamp", time);
+//
+//        String id = getStreamId(record, time);
+//
+//        instance.set(id, record, 0);
+//
+//        // load data, ensure is set
+//        JsonNode result = instance.get(id);
+//        assertNotNull(result);
+//
+//        instance.delete(id);
+//
+//        // load data, ensure is gone
+//        result = instance.get(id);
+//        assertNull(result);
+//
+//    }
+//
+//    /**
+//     * Test of list method, of class CouchbaseConnection.
+//     */
+//    @Test
+//    public void testList() {
+//
+//        int count = 5;
+//        int timestamp = createRecords(5);
+//
+//        // list records by userId + streamId
+//        BaseQuery query = BaseQuery.queryBy(
+//                new BaseQuery.QueryParam("userId", userId),
+//                new BaseQuery.QueryParam("streamId", streamId)
+//        );
+//
+//        List<JsonNode> result = instance.list(query);
+//
+//        assertEquals(result.size(), count);
+//
+//    }
+//
+//    /**
+//     * Test of list method, of class CouchbaseConnection.
+//     */
+//    @Test
+//    public void testListLimit() throws Exception {
+//
+//        int count = 5;
+//        int timestamp = createRecords(5);
+//
+//        // list records by userId + streamId
+//        BaseQuery query = BaseQuery.queryBy(
+//                new BaseQuery.QueryParam("userId", userId),
+//                new BaseQuery.QueryParam("streamId", streamId)
+//        );
+//
+//        query.limit = 1;
+//        query.setSort("timestamp", ListQuery.Sort.DESC);
+//
+//        List<JsonNode> result = instance.list(query);
+//
+//        assertEquals(result.get(0).get("timestamp").asInt(), timestamp);
+//
+//    }
+//
+//    private int createRecords(int count) {
+//
+//        int timestamp = 0;
+//
+//        for (int i = 0; i < count; i++) {
+//
+//            int time = getTime();
+//
+//            ObjectNode record = record = loadData("record");
+//
+//            timestamp = time + (i * 100);
+//
+//            record.put("timestamp", timestamp);
+//            record.put("userId", userId);
+//            record.put("streamId", streamId);
+//
+//            String id = getStreamId(record, timestamp);
+//            instance.set(id, record, 0);
+//
+//            JsonNode res = instance.get(id);
+//            assertNotNull(res);
+//
+//        }
+//
+//        return timestamp;
+//    }
 
 }
