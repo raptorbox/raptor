5a5398a79e6f6a1ce09ac273066e293b973a01c9
Luca Capra <luca.capra@gmail.com>
2017-02-02 16:43:37 +0100

    return created timestamp in seconds for consistency

--- raptor-auth-service/src/main/java/org/createnet/raptor/auth/service/services/JwtTokenService.java
+++ raptor-auth-service/src/main/java/org/createnet/raptor/auth/service/services/JwtTokenService.java
@@ -71,7 +71,7 @@ public class JwtTokenService implements Serializable {
     Map<String, Object> claims = new HashMap<>();
 
     claims.put(CLAIM_KEY_UUID, token.getUser().getUuid());
-    claims.put(CLAIM_KEY_CREATED, token.getCreated());
+    claims.put(CLAIM_KEY_CREATED, token.getCreated().getTime() / 1000L);
     
     String tokenValue = generateToken(claims, secret, expires);
     token.setToken(tokenValue);
