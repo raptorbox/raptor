b7867e9688c36468f6122b0330d681a74184b1f3
Luca Capra <luca.capra@gmail.com>
2017-02-09 14:44:16 +0100

    add title to swagger api

--- raptor-service/src/main/java/org/createnet/raptor/service/BaseAppConfig.java
+++ raptor-service/src/main/java/org/createnet/raptor/service/BaseAppConfig.java
@@ -17,6 +17,7 @@ package org.createnet.raptor.service;
 
 import io.swagger.jaxrs.config.BeanConfig;
 import io.swagger.models.Info;
+import io.swagger.models.License;
 import org.createnet.raptor.service.tools.RaptorApplicationEventListener;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -30,22 +31,32 @@ abstract public class BaseAppConfig extends ResourceConfig {
         register(RaptorApplicationEventListener.class);
         register(new ServiceBinder());
     }
-    
+
     public void registerSwagger(String resourcePackage) {
 
         register(io.swagger.jaxrs.listing.ApiListingResource.class);
         register(io.swagger.jaxrs.listing.SwaggerSerializers.class);
-        
+
         BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setTitle("Raptor HTTP API");
         beanConfig.setVersion("3.0");
         beanConfig.setSchemes(new String[]{"http", "https"});
         beanConfig.setHost("api.raptor.local");
+
         Info info = new Info();
         info.setTitle("Raptor API");
+        info.setDescription("The IoT platform");
+        info.setLicense(
+                new License()
+                        .name("Apache v2")
+                        .url("https://raw.githubusercontent.com/raptorbox/raptor/master/LICENSE")
+        );
+        info.setVersion("3.0");
+
         beanConfig.setInfo(info);
         beanConfig.setBasePath("/");
         beanConfig.setResourcePackage(resourcePackage);
         beanConfig.setScan(true);
     }
-    
+
 }
