78ac1dac13c423fdc0d00437882cf3a71d48496e
Luca Capra <luca.capra@gmail.com>
2017-07-15 13:47:11 +0200

    review paths

--- travis/install-nginx.sh
+++ travis/install-nginx.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-mkdir -p `pwd`/tmp/nginx/nginx_client_body
-mkdir -p `pwd`/tmp/nginx/nginx_fastcgi_temp
-mkdir -p `pwd`/tmp/nginx/nginx_proxy_temp
-mkdir -p `pwd`/tmp/nginx/nginx_scgi_temp
-mkdir -p `pwd`/tmp/nginx/nginx_uwsgi_temp
+mkdir -p /tmp/nginx/nginx_client_body
+mkdir -p /tmp/nginx/nginx_fastcgi_temp
+mkdir -p /tmp/nginx/nginx_proxy_temp
+mkdir -p /tmp/nginx/nginx_scgi_temp
+mkdir -p /tmp/nginx/nginx_uwsgi_temp
 
-mkdir -p `pwd`/tmp/nginx/sites
+mkdir -p /tmp/nginx/sites
 
-cp `pwd`/config/raptor.test.conf `pwd`/tmp/nginx/sites/raptor.test.conf
-cp `pwd`/travis/nginx.conf `pwd`/tmp/nginx/nginx.conf
+cp `pwd`/config/raptor.test.conf /tmp/nginx/sites/raptor.test.conf
+cp `pwd`/travis/nginx.conf /tmp/nginx/nginx.conf
